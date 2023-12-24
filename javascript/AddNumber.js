a9dd65e3e391c5da633805ca8c1af0ba8198e489
AryanSwami <swamiaryan611@gmail.com>
2023-12-24 14:31:14 +0530

    Add number

---
+++ javascript/AddNumber.js
@@ -0,0 +1,17 @@
+// fix values
+
+const a = 5;
+const b = 6;
+
+const Sum = a + b;
+
+// console.log("Sum of " + a + " and " + b + " is ", Sum);
+
+// input values
+
+const input1 = prompt("enter first number");
+const input2 = prompt("enter Second number");
+
+const result = input1 + input2;
+
+console.log("sum pf ${input1} and ${input2} is ${result}");
