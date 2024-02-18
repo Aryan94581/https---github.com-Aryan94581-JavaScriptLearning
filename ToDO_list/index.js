988bbc82741b675e1bde5cc3d9985d9187c7d217
AryanSwami <swamiaryan611@gmail.com>
2024-02-18 12:54:39 +0530

    mms

--- ToDO_list/index.js
+++ ToDO_list/index.js
@@ -1,11 +1,27 @@
-const ToDoList = [];
+const ToDoList = ["first task"];
+renderTodoList();
+
+function renderTodoList() {
+  let TodoListhtml = "";
+  const ToDoListElement = document.querySelector(".js-todo-list");
+
+  let i = 0;
+  while (i <= ToDoList.length) {
+    const todo = ToDoList[i];
+    const html = `
+  <p>${todo}</p>
+  `;
+    TodoListhtml += html;
+    i++;
+  }
+  ToDoListElement.innerHTML = TodoListhtml;
+}
 
 function AddTodo() {
   const inputElement = document.querySelector(".todo-input");
   const name = inputElement.value;
 
   ToDoList.push(name);
-  console.log(ToDoList);
-
   inputElement.value = "";
+  renderTodoList();
 }
